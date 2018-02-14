9ff034dab59938286b5ba35833a87298a4f46489
Garry Tsaconas <garrytsaconas@insightactioninc.com>
2018-02-13 22:12:48 -0500

    Added Tomorrow's forecast and styled a bit.

--- src/app/components/search-city/search-city.component.ts
+++ src/app/components/search-city/search-city.component.ts
@@ -22,17 +22,30 @@ interface CityWeather {
 		windBearing?: number,
 		windDirection?: string,
 		humidity?: number,
+		sunriseSunset?: {
+			sunrise?: any,
+			sunset?: any
+		}
 	},
-	today?: {
-		sunrise?: any,
-		sunset?: any
+	tomorrow?: {
+		summary?: string,
+		icon?: string,
+		temperature?: number,
+		windSpeed?: number,
+		windBearing?: number,
+		windDirection?: string,
+		humidity?: number,
+		sunriseSunset?: {
+			sunrise?: any,
+			sunset?: any
+		}
 	}
 }
 
 @Component({
   selector: 'app-search-city',
   templateUrl: './search-city.component.html',
-  styleUrls: ['./search-city.component.css']
+  styleUrls: ['./search-city.component.scss']
 })
 export class SearchCityComponent implements OnInit {
 	city: string = 'Toronto';
@@ -65,11 +78,24 @@ export class SearchCityComponent implements OnInit {
 			windSpeed: null,
 			windBearing: null,
 			windDirection: null,
-			humidity: null
+			humidity: null,
+			sunriseSunset: {
+				sunrise: null,
+				sunset: null
+			}
 		},
-		today: {
-			sunrise: null,
-			sunset: null
+		tomorrow: {
+			summary: null,
+			icon: null,
+			temperature: null,
+			windSpeed: null,
+			windBearing: null,
+			windDirection: null,
+			humidity: null,
+			sunriseSunset: {
+				sunrise: null,
+				sunset: null
+			}
 		}
 	};
 	isData: boolean = false;
@@ -80,27 +106,39 @@ export class SearchCityComponent implements OnInit {
   ngOnInit() {}
 
 	search() {
-		console.log(this.cityObj)
 		this.weather.getCities(this.cityObj).subscribe(result => {
 			this.result = result[0];
 
 			this.weather.getData(this.result.lat, this.result.lng).subscribe(weatherResult => {
-				console.log(weatherResult)
 				if (Object.keys(weatherResult).length > 0) {
 					this.isData = true;
 				} else {
 					return;
 				}
 
-				this.cityWeather.currently.time = this.convertUnixDate(weatherResult['currently'].time, true);
-				this.cityWeather.currently.summary = weatherResult['currently'].summary;
-				this.cityWeather.currently.icon = weatherResult['currently'].icon;
-				this.cityWeather.currently.temperature = Math.round(weatherResult['currently'].temperature);
-				this.cityWeather.currently.windSpeed = Math.round(weatherResult['currently'].windSpeed);
-				this.cityWeather.currently.windDirection = this.degToCompass(weatherResult['currently'].windBearing);
-				this.cityWeather.currently.humidity = weatherResult['currently'].humidity * 100;
-				this.cityWeather.today.sunrise = this.convertUnixDate(weatherResult['daily'].data[0].sunriseTime);
-				this.cityWeather.today.sunset = this.convertUnixDate(weatherResult['daily'].data[0].sunsetTime);
+				const today = weatherResult['currently'],
+							tomorrow = weatherResult['daily']['data'][1];
+
+				this.cityWeather.currently.time = this.convertUnixDate(today.time, true);
+				this.cityWeather.currently.summary = today.summary;
+				this.cityWeather.currently.icon = today.icon;
+				this.cityWeather.currently.temperature = Math.round(today.temperature);
+				this.cityWeather.currently.windSpeed = Math.round(today.windSpeed);
+				this.cityWeather.currently.windDirection = this.degToCompass(today.windBearing);
+				this.cityWeather.currently.humidity = today.humidity * 100;
+				this.cityWeather.currently.sunriseSunset.sunrise = this.convertUnixDate(weatherResult['daily'].data[0].sunriseTime);
+				this.cityWeather.currently.sunriseSunset.sunset = this.convertUnixDate(weatherResult['daily'].data[0].sunsetTime);
+
+				console.log(weatherResult['daily']['data'][1])
+
+				this.cityWeather.tomorrow.summary = tomorrow.summary;
+				this.cityWeather.tomorrow.icon = tomorrow.icon;
+				this.cityWeather.tomorrow.temperature = Math.round(tomorrow.temperatureHigh);
+				this.cityWeather.tomorrow.windSpeed = Math.round(tomorrow.windSpeed);
+				this.cityWeather.tomorrow.windDirection = this.degToCompass(today.windBearing);
+				this.cityWeather.tomorrow.humidity = tomorrow.humidity * 100;
+				this.cityWeather.tomorrow.sunriseSunset.sunrise = this.convertUnixDate(weatherResult['daily'].data[1].sunriseTime);
+				this.cityWeather.tomorrow.sunriseSunset.sunset = this.convertUnixDate(weatherResult['daily'].data[1].sunsetTime);
 
 			});
 		});
@@ -119,10 +157,14 @@ export class SearchCityComponent implements OnInit {
 
 	weatherIcon(icon) {
     switch (icon) {
+			case 'clear-night':
+				return 'wi wi-night-clear';
       case 'partly-cloudy-day':
         return 'wi wi-day-cloudy'
 			case 'cloudy':
 				return 'wi wi-cloudy';
+			case 'fog':
+				return 'wi wi-fog';
 			case 'snow':
 				return 'wi wi-snow';
       case 'clear-day':
