57c8db42c9e015675471efa2622a637ee9ba7a4b
Garry T <incarnate@rogers.com>
2018-02-26 17:14:37 -0500

    Con't attempts at Responsiveness. Refined the code: rounded Temperature Y-Axis figures.

--- src/app/components/weather-chart/weather-chart.component.ts
+++ src/app/components/weather-chart/weather-chart.component.ts
@@ -49,9 +49,7 @@ export class WeatherChartComponent implements OnInit, OnChanges {
 
     // In order to achieve the X-Axis Ticks to start at 0,0, scale using scaleTime() and
     // update the Data using UNIX Timestamps to a Moment Object.
-    this.data.forEach((d) => {
-      d['time'] = moment.unix(d['time']).startOf('day');
-    });
+    this.data.forEach(d => d['time'] = moment.unix(d['time']).startOf('day'));
 
     // Find and Assign the Min and Max X-Axis range.
     const xMin = d3.min(this.data, d => Math.min(d['time'])),
@@ -64,8 +62,8 @@ export class WeatherChartComponent implements OnInit, OnChanges {
 
     // Draw the Line.
     this.line = d3.line()
-        .x((d) => this.xScale(d['time']))
-        .y((d) => this.yScale(d['temperature']));
+									.x(d => this.xScale(d['time']))
+									.y(d => this.yScale(d['temperature']));
   }
 
   createLineChart() {
@@ -78,12 +76,17 @@ export class WeatherChartComponent implements OnInit, OnChanges {
     const chartElem = d3.select(element),
           aspect = this.width / this.height;
 
-    this.svg = chartElem.append("div")
-      .classed("svg-container", true)
+    this.svg = chartElem.append('div')
+      .classed('svg-container', true)
       .append('svg')
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", `0 0 ${element.offsetWidth} ${element.offsetHeight}`)
-      .classed("svg-content-responsive", true);
+      .attr('preserveAspectRatio', 'xMinYMin meet')
+      .attr('viewBox', `0 0 ${element.offsetWidth} ${element.offsetHeight}`);
+
+    d3.select(window).on('resize.updatesvg', resize);
+
+    function resize() {
+
+    }
 
     // Chart out the Plot Area.
     this.chart = this.svg
@@ -96,73 +99,73 @@ export class WeatherChartComponent implements OnInit, OnChanges {
 
     // Add the X Axis
     this.svg
-        .append("g")
-        .attr("transform", `translate(50, ${this.height})`)
+        .append('g')
+        .attr('transform', `translate(50, ${this.height})`)
         .attr('class', 'x-axis')
         .call(d3.axisBottom(this.xScale).ticks(d3.timeDay));
 
     // Add the Y Axis
     this.svg
-        .append("g")
+        .append('g')
         .attr('transform', `translate(50, 0)`)
         .attr('class', 'y-axis')
-        .call(d3.axisLeft(this.yScale));
+        .call(d3.axisLeft(this.yScale).tickFormat(d3.format('d')));
 
     // Add the Line path.
     this.svg
-        .append("path")
-        .attr("class", "weather-line")
+        .append('path')
+        .attr('class', 'weather-line')
         .attr('transform', `translate(50, 0)`)
         .data([this.data])
-        .attr("d", this.line);
+        .attr('d', this.line);
 
     // Add titles to the X-Axis.
     this.svg
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("transform", `translate(${this.width / 2}, ${this.height + 50})`)
-        .text("Date");
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('transform', `translate(${this.width / 2}, ${this.height + 50})`)
+        .text('Date');
 
     // Add titles to the Y-Axis.
     this.svg
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("transform", `translate(5, ${this.height / 2})rotate(-90)`)
-        .html("Temperature (&deg C)");
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('transform', `translate(5, ${this.height / 2})rotate(-90)`)
+        .html('Temperature (&deg C)');
 
     // Define the div for the tooltip
-    const div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+    const div = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
 
     // Draw the Dots on the Chart.
     this.svg
-      .selectAll(".dot")
+      .selectAll('.dot')
       .data(this.data.filter(d => d))
       .enter()
-        .append("circle")
+        .append('circle')
         .attr('transform', `translate(50, 0)`)
-        .attr("class", "dot")
-        .attr("cx", (d) => this.xScale(d['time']))
-        .attr("cy", (d) => this.yScale(d['temperature']))
-        .attr("r", 4.5)
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout);
+        .attr('class', 'dot')
+        .attr('cx', d => this.xScale(d['time']))
+        .attr('cy', d => this.yScale(d['temperature']))
+        .attr('r', 4.5)
+        .on('mouseover', mouseover)
+        .on('mouseout', mouseout);
 
     // Hover functionality.
     function mouseover(d) {
       div.transition()
          .duration(this.hoverDuration)
-         .style("opacity", .9);
+         .style('opacity', .9);
       div.html(`<span>${d['time'].format('ddd MMM DD')}</span> ${d['temperature']}&deg`)
-         .style("left", `${d3.event.pageX - 75}px`)
-         .style("top", `${d3.event.pageY + 10}px`);
+         .style('left', `${d3.event.pageX - 75}px`)
+         .style('top', `${d3.event.pageY + 10}px`);
       };
 
     function mouseout() {
       div.transition()
          .duration(this.hoverDuration)
-         .style("opacity", 0);
+         .style('opacity', 0);
     }
   }
 
@@ -174,16 +177,16 @@ export class WeatherChartComponent implements OnInit, OnChanges {
         .select('.y-axis')
         .transition()
         .duration(this.updateDuration)
-        .call(d3.axisLeft(this.yScale));
+        .call(d3.axisLeft(this.yScale).tickFormat(d3.format('d')));
 
     // Update the Line.
     this.svg
-        .select("path.weather-line")
+        .select('path.weather-line')
         .data([this.data])
         .transition()
         .duration(this.updateDuration)
         .attr('transform', `translate(50, 0)`)
-        .attr("d", this.line);
+        .attr('d', this.line);
 
     // Update the Points on the Chart.
     this.svg
@@ -192,8 +195,8 @@ export class WeatherChartComponent implements OnInit, OnChanges {
         .transition()
         .duration(this.updateDuration)
         .attr('transform', `translate(50, 0)`)
-        .attr("cx", (d) => this.xScale(d['time']))
-        .attr("cy", (d) => this.yScale(d['temperature']))
-        .attr("r", 4.5);
+        .attr('cx', d => this.xScale(d['time']))
+        .attr('cy', d => this.yScale(d['temperature']))
+        .attr('r', 4.5);
   }
 }
